d2ab1916a29f1617e29b2d90adbb4b03043a3a64
rafatmyo <rafatmyo@gmail.com>
2020-07-04 12:43:51 -0500

    Added: Video List

---
+++ src/components/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+const KEY = 'AIzaSyDzItdKVDVpiwC72PL9r66uYcimLtZkx60'
+
+class App extends React.Component {
+    state = {videos: []}
+
+    onTermSubmit = async term => {
+        const response = await youtube.get("/search", {
+            params: {
+                q: term,
+                part: "snippet",
+                maxResults: 5,
+                key: KEY
+            }
+        });
+
+        this.setState({videos: response.data.items });
+    };
+
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
+}
+
+export default App;
